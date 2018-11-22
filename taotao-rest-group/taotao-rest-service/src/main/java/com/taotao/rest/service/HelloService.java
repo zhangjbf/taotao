03254606682957a95fd8a4ad494e9e78ab5f28ac
jiabin.zhang <jiabin.zhang@rograndec.com>
2018-11-22 21:23:25 +0800

    123

---
+++ taotao-rest-group/taotao-rest-service/src/main/java/com/taotao/rest/service/HelloService.java
@@ -0,0 +1,13 @@
+package com.taotao.rest.service;
+
+/**
+ * @Version: 1.0
+ * @Author: jiabin.zhang 张佳宾
+ * @Email: jiabin.zhang@rograndec.com
+ * @CreateDate 2018/11/21
+ */
+public interface HelloService {
+
+    public String sayHello();
+
+}
